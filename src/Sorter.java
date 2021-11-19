83451060b0aa4b6d6e47380cb7e7c1aa3b54c7db
zerbash <dbasham@overlake.org>
2021-11-19 09:49:42 -0800

    11-19-21

--- src/Sorter.java
+++ src/Sorter.java
@@ -4,6 +4,12 @@ import java.util.Random;
 public class Sorter {
 	private int[] nums;
 
+	public int getCount() {
+		return count;
+	}
+
+	private int count;
+
 	Sorter(int length) {
 		nums = new int[length];
 		Random random = new Random();
@@ -14,6 +20,7 @@ public class Sorter {
 
 	public void bogoSort() {
 		while (!isSorted()) {
+			count++;
 			shuffle();
 		}
 	}
@@ -29,7 +36,7 @@ public class Sorter {
 
 	public void shuffle() {
 		Random random = new Random();
-		for (int i = nums.length - 1; i >= 0; i--) {
+		for (int i = nums.length - 1; i > 0; i--) {
 			int randomIndex = random.nextInt(i);
 			int temp = nums[randomIndex];
 			nums[randomIndex] = nums[i];
@@ -40,5 +47,23 @@ public class Sorter {
 	@Override
 	public String toString() {
 		return Arrays.toString(nums);
+	}
+
+	public void mergeSort() {
+
+	}
+
+	private void merge(int[] a, int[] left, int[] right) {
+		int leftIndex = 0;
+		int rightIndex = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (rightIndex >= right.length || leftIndex < left.length && left[leftIndex] < right[rightIndex]) {
+				a[i] = left[leftIndex];
+				leftIndex++;
+			} else {
+				a[i] = right[rightIndex];
+				rightIndex++;
+			}
+		}
 	}
 }
--- src/SorterClient.java
+++ src/SorterClient.java
@@ -1,8 +1,10 @@
 public class SorterClient {
 	public static void main(String[] args) {
-		Sorter sorter = new Sorter(10);
+		Sorter sorter = new Sorter(11);
 		System.out.println(sorter);
-		sorter.bogoSort();
+		sorter.mergeSort();
+		//sorter.bogoSort();
 		System.out.println(sorter);
+		System.out.println("The sort took " + sorter.getCount() + " \"iterations\"");
 	}
 }
